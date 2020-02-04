1aeaece3715d6ce6f0bdbe63a5ff0493d8c69578
Chris <csgephardt@gmail.com>
2020-02-04 11:05:04 -0500

    Add IClickableMenu Rewrites

--- src/SMAPI/Framework/RewriteFacades/IClickableMenuMethods.cs
+++ src/SMAPI/Framework/RewriteFacades/IClickableMenuMethods.cs
@@ -14,30 +14,41 @@ namespace StardewModdingAPI.Framework.RewriteFacades
         {
             drawHoverText(b, text, font, xOffset, yOffset, moneyAmounttoDisplayAtBottom, boldTitleText, healAmountToDisplay, buffIconsToDsiplay, hoveredItem, currencySymbol, extraItemToShowIndex, extraItemToShowAmount, overideX, overrideY, alpha, craftingIngrediants, -1, 80, -1, additional_craft_materials);
         }
+
         [SuppressMessage("ReSharper", "CS0109", Justification = "The 'new' modifier applies when compiled on Windows.")]
         public static new void drawTextureBox(SpriteBatch b, Texture2D texture, Microsoft.Xna.Framework.Rectangle sourceRect, int x, int y, int width, int height, Color color)
         {
             drawTextureBox(b, texture, sourceRect, x, y, width, height, color, 1, true, false);
         }
+
         [SuppressMessage("ReSharper", "CS0109", Justification = "The 'new' modifier applies when compiled on Windows.")]
         public static new void drawTextureBox(SpriteBatch b, Texture2D texture, Microsoft.Xna.Framework.Rectangle sourceRect, int x, int y, int width, int height, Color color, float scale)
         {
             drawTextureBox(b, texture, sourceRect, x, y, width, height, color, scale, true, false);
         }
+
         [SuppressMessage("ReSharper", "CS0109", Justification = "The 'new' modifier applies when compiled on Windows.")]
         public static new void drawTextureBox(SpriteBatch b, Texture2D texture, Microsoft.Xna.Framework.Rectangle sourceRect, int x, int y, int width, int height, Color color, float scale, bool drawShadow)
         {
             drawTextureBox(b, texture, sourceRect, x, y, width, height, color, scale, drawShadow, false);
         }
+
         [SuppressMessage("ReSharper", "CS0109", Justification = "The 'new' modifier applies when compiled on Windows.")]
         public new void drawHorizontalPartition(SpriteBatch b, int yPosition, bool small = false, int red = -1, int green = -1, int blue = -1)
         {
             this.drawMobileHorizontalPartition(b, 0, yPosition, 64, small);
         }
+
         [SuppressMessage("ReSharper", "CS0109", Justification = "The 'new' modifier applies when compiled on Windows.")]
         public new void drawVerticalUpperIntersectingPartition(SpriteBatch b, int xPosition, int partitionHeight, int red = -1, int green = -1, int blue = -1)
         {
             this.drawVerticalUpperIntersectingPartition(b, xPosition, partitionHeight);
+        }
+
+        [SuppressMessage("ReSharper", "CS0109", Justification = "The 'new' modifier applies when compiled on Windows.")]
+        public new void drawVerticalIntersectingPartition(SpriteBatch b, int xPosition, int yPosition, int red = -1, int green = -1, int blue = -1)
+        {
+            this.drawVerticalIntersectingPartition(b, xPosition, yPosition);
         }
     }
 }
