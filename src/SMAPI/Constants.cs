59ffb3c57b70e81275ac34a42a58f9b09712eba2
Chris <csgephardt@gmail.com>
2020-02-04 11:04:41 -0500

    Remove AndroidApiVersion Constant and bump version to 3.2.0.1

--- src/SMAPI/Constants.cs
+++ src/SMAPI/Constants.cs
@@ -20,10 +20,7 @@ namespace StardewModdingAPI
         ** Public
         ****/
         /// <summary>SMAPI's current semantic version.</summary>
-        public static ISemanticVersion ApiVersion { get; } = new Toolkit.SemanticVersion("3.2.0");
-
-        /// <summary>Android SMAPI's current semantic version.</summary>
-        public static ISemanticVersion AndroidApiVersion { get; } = new Toolkit.SemanticVersion("3.1.0-experimental");
+        public static ISemanticVersion ApiVersion { get; } = new Toolkit.SemanticVersion("3.2.0.1", allowNonStandard: true);
 
         /// <summary>The minimum supported version of Stardew Valley.</summary>
         public static ISemanticVersion MinimumGameVersion { get; } = new GameVersion("1.4.1");
@@ -98,7 +95,7 @@ namespace StardewModdingAPI
         internal static string ModsPath { get; set; }
 
         /// <summary>The game's current semantic version.</summary>
-        internal static ISemanticVersion GameVersion { get; } = new GameVersion("1.3.36");
+        internal static ISemanticVersion GameVersion { get; } = new GameVersion("1.4.4.122");
 
         /// <summary>The target game platform.</summary>
         internal static Platform Platform { get; } = Platform.Android;
