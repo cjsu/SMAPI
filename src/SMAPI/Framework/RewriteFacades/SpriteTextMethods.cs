4a5a8ef7b369725049eed1eef738501c6f57b25f
yangzhi <@4F!xZpJwly&KbWq>
2020-02-14 13:00:57 +0800

    Update to latest 1.4.5.136 game version

--- src/SMAPI/Framework/RewriteFacades/SpriteTextMethods.cs
+++ src/SMAPI/Framework/RewriteFacades/SpriteTextMethods.cs
@@ -14,14 +14,14 @@ namespace StardewModdingAPI.Framework.RewriteFacades
             return getWidthOfString(s);
         }
 
-        public static void drawStringHorizontallyCenteredAt(SpriteBatch b, string s, int x, int y, int characterPosition = 999999, int width = -1, int height = 999999, float alpha = -1f, float layerDepth = 0.88f, bool junimoText = false, int color = -1, int maxWdith = 99999)
+        public static void drawStringHorizontallyCenteredAt(SpriteBatch b, string s, int x, int y, int characterPosition = 999999, int width = -1, int height = 999999, float alpha = -1f, float layerDepth = 0.088f, bool junimoText = false, int color = -1, int maxWdith = 99999)
         {
             drawString(b, s, x - SpriteText.getWidthOfString(s) / 2, y, characterPosition, width, height, alpha, layerDepth, junimoText, -1, "", color);
         }
 
         public static void drawStringWithScrollBackground(SpriteBatch b, string s, int x, int y, string placeHolderWidthText = "", float alpha = 1f, int color = -1, SpriteText.ScrollTextAlignment scroll_text_alignment = SpriteText.ScrollTextAlignment.Left)
         {
-            SpriteText.drawString(b, s, x, y, 999999, -1, 999999, alpha, 0.88f, false, 0, placeHolderWidthText, color, scroll_text_alignment);
+            SpriteText.drawString(b, s, x, y, 999999, -1, 999999, alpha, 0.088f, false, 0, placeHolderWidthText, color, scroll_text_alignment);
         }
 
         public static void drawStringWithScrollBackground(SpriteBatch b, string s, int x, int y, string placeHolderWidthText, float alpha, int color)
